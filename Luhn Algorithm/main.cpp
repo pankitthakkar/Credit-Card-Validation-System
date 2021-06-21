ae9ddc12790e3d991cac989c569fe5e26990c81a
Pankit Thakkar <pankitthakkar007@gmail.com>
2021-06-21 03:40:20 -0400

    Added Integer Validation

--- Luhn Algorithm/main.cpp
+++ Luhn Algorithm/main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 #include <string>
+
+using namespace std;
+
+bool IsNumber(const string& s) {
+    int len = s.length();
+    for (int i = 0; i < len; i++) {
+        if (s[i] < '0' || s[i] > '9')
+            return false;
+    }
+    return true;
+}
